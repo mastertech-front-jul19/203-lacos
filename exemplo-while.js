3c633bf3a6a168b7e844c0027bf5bfcc88b70b81
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-12 20:58:07 -0300

    Adicionar exemplo de while

---
+++ exemplo-while.js
@@ -0,0 +1,27 @@
+console.log('Iniciou o programa');
+
+// Laços
+
+// Contador
+let i = 0;
+// Condição
+while (i < 10) {
+    console.log(i);
+
+    // Passo
+    // contador = contador + 1;
+    // contador += 1;
+    i ++;
+}
+
+let j = 10;
+
+while(j > 0) {
+    console.log(j);
+
+    // contador2 = contador2 - 1;
+    // contador2 -= 1;
+    j --;
+}
+
+console.log('Fim do código');
