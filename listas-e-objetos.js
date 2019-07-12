fc330cfb628dbc1055846c6b88c4ceabf1fa4e0b
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-12 20:17:55 -0300

    Adicionar exemplos de lista e objeto

---
+++ listas-e-objetos.js
@@ -0,0 +1,20 @@
+
+let lista = ["","",""];
+
+let objeto = {
+    nome: "Felipe",
+    idade: 24,
+    vivo: false,
+    programador: "Mais ou menos"
+}
+
+
+let casa = {
+    quarto: {
+        listaItems: ["Cama", "Colchão", "Guarda-Roupa", "Daiane"],
+        corDasParedes: "Azul",
+        portas: 2,
+        banheiro: true
+    },
+    piscina: false
+}
